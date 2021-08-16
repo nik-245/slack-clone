166ee31dc9bde866b9f09c528daa9d3731e33fcc
nikunj ladva <nikunjladva245@gmail.com>
2021-08-16 20:45:48 +0530

    add config function and auth function

--- slack/src/firebase.js
+++ slack/src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: "AIzaSyCLLya98EepQlJz6HJvLUeM1vCCLM43Vi8",
+    authDomain: "slack-clone-11636.firebaseapp.com",
+    projectId: "slack-clone-11636",
+    storageBucket: "slack-clone-11636.appspot.com",
+    messagingSenderId: "595471625106",
+    appId: "1:595471625106:web:02398484d40fe379e56f4b"
 };
 
 
