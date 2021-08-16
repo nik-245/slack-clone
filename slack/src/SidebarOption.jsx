3befbd2e80d3f22c3da89db596dc6d29180e2c3d
nikunj ladva <nikunjladva245@gmail.com>
2021-08-16 19:23:49 +0530

    add firebase.js for connection with the our app

--- slack/src/SidebarOption.jsx
+++ slack/src/SidebarOption.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 import "./SidebarOption.css";
-const SidebarOption = ({Icon , title }) => {
-    return (
-        <div className="sidebaroption">
-            {Icon && <Icon  className="sidebaroption_icon"/>}
-        </div>
-    )
-}
+const SidebarOption = ({ Icon, title }) => {
+  return (
+    <div className="sidebaroption">
+      {Icon && <Icon className="sidebaroption_icon" />}
+      {Icon ? (
+        <h3>{title}</h3>
+      ) : (
+        <h3 className="sidebaroption_channal">
+          <span className="sidebaroption_hash">#</span>
+          {title}
+        </h3>
+      )}
+    </div>
+  );
+};
 
 export default SidebarOption;
